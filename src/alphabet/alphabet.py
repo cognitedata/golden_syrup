6159564930e9b3ab3b99a5f2ada93e979cb4f0db
Magnus Moan <magnus.moan@cognite.com>
2020-08-11 10:35:43 +0200

    Initial commit

---
+++ src/alphabet/alphabet.py
@@ -0,0 +1,20 @@
+from typing import Tuple, List
+
+
+def find_vowels(s: str) -> str:
+    raise NotImplementedError("find_vowels not implemented yet")
+
+
+def find_consonants(s: str) -> str:
+    raise NotImplementedError("find_consonants not implemented yet")
+
+
+def filter_strings(file_path: str) -> List[Tuple[str, str]]:
+    """
+    1. Reads content from file_path
+    2. For each line in the content, run find_vowels and find_constants
+    3. The results from each line should be stored as a tuple with the result
+       from find_vowels as the first element in the tuple
+    4. Return the results as a list of tuples
+    """
+    raise NotImplementedError("filter_strings not implemented yet")
