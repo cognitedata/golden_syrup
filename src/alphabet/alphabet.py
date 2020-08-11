a607973d7c88b68d90e16403e47f21aa5484f032
Magnus Moan <magnus.moan@cognite.com>
2020-08-11 10:53:12 +0200

    Simplifying

--- src/alphabet/alphabet.py
+++ src/alphabet/alphabet.py
@@ -9,12 +9,11 @@ def find_consonants(s: str) -> str:
     raise NotImplementedError("find_consonants not implemented yet")
 
 
-def filter_strings(file_path: str) -> List[Tuple[str, str]]:
+def filter_strings(strings: List[str]) -> List[Tuple[str, str]]:
     """
-    1. Reads content from file_path
-    2. For each line in the content, run find_vowels and find_constants
-    3. The results from each line should be stored as a tuple with the result
+    1. For each string in strings, run find_vowels and find_constants
+    2. The results from each line should be stored as a tuple with the result
        from find_vowels as the first element in the tuple
-    4. Return the results as a list of tuples
+    3. Return the results as a list of tuples
     """
     raise NotImplementedError("filter_strings not implemented yet")
