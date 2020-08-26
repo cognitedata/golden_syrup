f44c87833fa1a51536732cfe79a624ac839ca736
Håkon V. Treider <haakonvt@gmail.com>
2020-08-26 09:37:07 +0200

    typo ;)

--- src/alphabet/alphabet.py
+++ src/alphabet/alphabet.py
@@ -11,7 +11,7 @@ def find_consonants(s: str) -> str:
 
 def filter_strings(strings: List[str]) -> List[Tuple[str, str]]:
     """
-    1. For each string in strings, run find_vowels and find_constants
+    1. For each string in strings, run find_vowels and find_consonants
     2. The results from each line should be stored as a tuple with the result
        from find_vowels as the first element in the tuple
     3. Return the results as a list of tuples
